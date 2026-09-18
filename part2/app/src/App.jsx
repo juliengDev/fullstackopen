d1afd5f7467bb256dccc080bdebd5935fdfaefa4
Julien Gilbert <juliengilbert.dev@gmail.com>
2026-09-18 12:11:29 +0200

    2.12 - 2.13 done

--- part2/app/src/App.jsx
+++ part2/app/src/App.jsx
@@ -1,71 +1,97 @@
-import { useState, useEffect } from "react";
-import Note from "./components/Note";
-import axios from "axios";
-import noteService from "./services/note";
+import { useEffect, useState } from "react";
+import Searchbar from "../../phonebook/Searchbar";
+import AddNewPerson from "../../phonebook/AddNewPerson";
+import Contacts from "../../phonebook/Contacts";
+import personsService from "./services/note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", date: new Date().toISOString(), id: 6364662 },
+    { name: "Ada Lovelace", number: "39-44-5323523", date: new Date().toISOString(), id: 1145636 },
+    { name: "Dan Abramov", number: "12-43-234345", date: new Date().toISOString(), id: 9409711 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", date: new Date().toISOString(), id: 8135745 },
+  ]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  useEffect(() => {
-    noteService.getAll().then((initialNotes) => {
-      setNotes(initialNotes);
-    });
+  useEffect(function () {
+    personsService.getAll().then((data) => setPersons(data));
   }, []);
 
-  const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3002/notes/${id}`;
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
+  const isNameAlreadyExistLocally = (name) => {
+    const nameInput = document.getElementById("name");
+    const isExist = persons.filter((person) => person.name === name);
+    nameInput.value = "";
 
-    noteService
-      .update(id, changedNote)
-      .then((returnedNote) => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-      })
-      .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
-        setNotes(notes.filter((n) => n.id !== id));
-      });
+    return isExist.length === 0 ? false : true;
+  };
+  // const isNameAlreadyExistOnServer = (newObj) => {
+  //   const nameInput = document.getElementById("name");
+
+  //   const person = personsService.getOnly(newObj.id).catch((error) => console.log(error));
+
+  // };
+  const handleChangeName = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
-  const addNote = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const noteObject = {
-      content: newNote,
+
+    const nameInput = document.getElementById("name");
+    const numberInput = document.getElementById("number");
+
+    if (isNameAlreadyExistLocally(newName)) {
+      alert(`${newName} is already added to local phonebook`);
+      return;
+    }
+
+    const nameObj = {
+      name: newName,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      number: newNumber,
+      id: Math.floor(Math.random() * 10000000),
     };
 
-    noteService.create(noteObject).then((returnedNote) => {
-      setNotes(notes.concat(returnedNote));
-      setNewNote("");
+    personsService.create(nameObj).then((returnPerson) => {
+      setPersons(persons.concat(nameObj));
+      setNewName("");
+      setNewNumber("");
+      nameInput.value = "";
+      numberInput.value = "";
     });
   };
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
+  const handleSearch = (event) => {
+    const value = event.target.value.toLowerCase().trim();
+    setSearchTerm(value);
   };
+  const filteredPerson = persons.filter((person) => person.name.toLowerCase().trim().includes(searchTerm));
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const handleDelete = (id) => {
+    const personFind = persons.find((person) => person.id === id);
+    console.log(personFind);
+
+    personsService
+      .remove(personFind.id)
+      .then((response) => {
+        console.log(`utilisateur : ${personFind.name} - ${personFind.id} retirée`);
+        const newPersons = persons.filter((p) => p.id !== personFind.id);
+        setPersons(person.contact(newPersons));
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>show {showAll ? "important" : "all"}</button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      <h2>Phonebook</h2>
+      <Searchbar handleSearch={handleSearch} />
+      <AddNewPerson handleSubmit={handleSubmit} handleChangeName={handleChangeName} handleChangeNumber={handleChangeNumber} />
+      <Contacts filteredPerson={filteredPerson} handleDelete={handleDelete} />
     </div>
   );
 };
--- part2/app/src/services/note.js
+++ part2/app/src/services/note.js
@@ -1,19 +1,16 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3002/notes";
+const baseUrl = "http://localhost:3002/persons";
 
 const getAll = () => {
-  // const request = axios.get(baseUrl);
-  // return request.then((response) => response.data);
   const request = axios.get(baseUrl);
-  const nonExisting = {
-    id: 10000,
-    content: "This note is not saved to server",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  };
-  return request.then((response) => response.data.concat(nonExisting));
+
+  return request.then((response) => response.data);
 };
 
+const getOnly = (id) => {
+  const request = axios.get(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
   return request.then((response) => response.data);
@@ -24,8 +21,15 @@ const update = (id, newObject) => {
   return request.then((response) => response.data);
 };
 
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
 export default {
   getAll,
+  getOnly,
   create,
   update,
+  remove,
 };
